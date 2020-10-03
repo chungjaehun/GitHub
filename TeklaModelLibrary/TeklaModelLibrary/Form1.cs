1822ed2c9b912c478dec17dc03d2050c2b51da77
재훈 정 <wognswjd70@naver.com>
2020-10-03 08:34:23 +0800

    버튼 타이틀 변경

--- TeklaModelLibrary/TeklaModelLibrary/Form1.cs
+++ TeklaModelLibrary/TeklaModelLibrary/Form1.cs
@@ -15,6 +15,12 @@ namespace TeklaModelLibrary
         public Form1()
         {
             InitializeComponent();
+            button1.Text = "OK";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
